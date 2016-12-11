2493bb711626743891befb24427861f8cc6e12ac
Will Timpson <will.timpson@gmail.com>
2016-12-11 00:20:15 -0500

    Addition Functions
    finbase.py
    * add_feed:
      * Attempts to fetch a title if you don't pass one.
      * Attaches categories if you pass them.
    * add_category:
      * Adds feeds if you pass them.

--- finbase.py
+++ finbase.py
@@ -1,11 +1,12 @@
 from pony import orm
 from datetime import datetime
+import feedparser
 
 db = orm.Database()
 
 class Feed(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
-    title = orm.Required(str, unique=True)
+    title = orm.Required(str)
     url = orm.Required(str)
     updated = orm.Optional(datetime)
     queried = orm.Optional(datetime)
@@ -23,10 +24,35 @@ class Article(db.Entity):
 
 class Category(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
-    title = orm.Required(str)
+    title = orm.Required(str, unique=True)
     feeds = orm.Set('Feed')
 
 def setup():
     orm.sql_debug(True)
     db.bind('sqlite', 'fbdb.sqlite', create_db=True)
     db.generate_mapping(create_tables=True)
+
+@orm.db_session
+def add_feed(url, title='', categories=None):
+    if not title:
+        p = feedparser.parse(url)
+        if 'title' in p.feed:
+            title = p.feed.title
+        else:
+            title = url
+
+    if categories:
+        new_feed = Feed(title=title, url=url, categories=categories)
+    else:
+        new_feed = Feed(title=title, url=url)
+
+    return new_feed
+
+@orm.db_session
+def add_category(title, feeds=None):
+    if feeds:
+        new_category = Category(title=title, feeds=feeds)
+    else:
+        new_category = Category(title=title)
+
+    return new_category
