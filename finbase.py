e64ae22040b1d49ca768de655bb1a2206481a644
Will Timpson <will.timpson@gmail.com>
2016-12-13 21:42:22 -0500

    Parse Feeds
    finbase.py
    * fetch_feed(id): given a feed id, feedparser.parse its url and
      add new Articles to the database, creating Tags and Authors as
      necessary. Update db fields for updated articles.
    * fetch_all_feeds(): iterate over Feeds call fetch_feed().
    * Fix typos, update db schema as necessary.

--- finbase.py
+++ finbase.py
@@ -10,25 +10,24 @@ class Feed(db.Entity):
     categories = orm.Set('Category')
     title = orm.Required(str)
     url = orm.Required(str, unique=True)
-    etag = orm.Optional(str)
-    modified = orm.Optional(str)
-
+    etag = orm.Required(str)
+    modified = orm.Required(str)
 
 class Article(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     feed = orm.Required('Feed')
-    authors = orm.Set('Author')
+    author = orm.Required('Author')
     tags = orm.Set('Tag')
     title = orm.Required(str)
     url = orm.Required(str, unique=True)
     read = orm.Required(bool, default=False)
-    published = orm.Optional(datetime)
-    summary = orm.Optional(str)
+    published = orm.Required(datetime)
+    summary = orm.Required(str)
 
 class Category(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     title = orm.Required(str, unique=True)
-    feeds = orm.Set('Feed')
+    feeds = orm.Optional('Feed')
 
 class Tag(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
@@ -45,16 +44,48 @@ def setup():
     db.bind('sqlite', 'fbdb.sqlite', create_db=True)
     db.generate_mapping(create_tables=True)
 
+@orm.db_session
+def fetch_all_feeds():
+    for feed_id in orm.select(f.id for f in Feed):
+        fetch_feed(feed_id)
 
 @orm.db_session
-def add_feed(url):
-    exists = Feed.get(url=url)
-    if exists:
-        return exists.id
-    else:
-        new_feed = Feed(title=title, url=url)
-        commit()
-        return new_feed.id
+def fetch_feed(id):
+    feed = Feed[id]
+    p = feedparser.parse(feed.url, etag=feed.etag, modified=feed.modified)
+    feed.etag = p.etag if 'etag' in p else ''
+    feed.modified = p.modified if 'modified' in p else ''
+    for e in p.entries if 'entries' in p else []:
+        author = Author[add_author(e.author)] if 'author' in e and e.author else Author[add_author('none')]
+        tags = [Tag[add_tag(t.term)] for t in e.tags] if 'tags' in e else Tag[add_tag('none')]
+        title = e.title if 'title' in e else ''
+        url = e.link if 'link' in e else feed.url
+        published = datetime(*e.published_parsed[:6]) if 'published_parsed' in e else datetime.now()
+        summary = e.summary if 'summary' in e else ''
+        a = Article.get(url=url)
+        if a:
+            if a.published != published:
+                a.feed = feed
+                a.author = author
+                a.tags = tags
+                a.title = title
+                a.published = published
+                a.summary = summary
+        else:
+            new_article = Article(feed=feed, author=author, tags=tags, title=title, url=url, published=published, summary=summary)
+
+def add_feed(url, title=''):
+    if not title:
+        p = feedparser.parse(url)
+        title = p.feed.title if 'title' in p.feed else url
+    with orm.db_session:
+        exists = Feed.get(url=url)
+        if exists:
+            return exists.id
+        else:
+            new_feed = Feed(title=title, url=url)
+            orm.commit()
+            return new_feed.id
 
 @orm.db_session
 def del_feed(id):
@@ -67,7 +98,7 @@ def add_category(title):
         return exists.id
     else:
         new_category = Category(title=title)
-        commit()
+        orm.commit()
         return new_category.id
 
 @orm.db_session
@@ -81,7 +112,7 @@ def add_tag(label):
         return exists.id
     else:
         new_tag = Tag(label=label)
-        commit()
+        orm.commit()
         return new_tag.id
 
 @orm.db_session
@@ -90,12 +121,12 @@ def del_tag(id):
 
 @orm.db_session
 def add_author(name):
-    exists=Author.get(name)
+    exists=Author.get(name=name)
     if exists:
         return exists.id
     else:
         new_author = Author(name=name)
-        commit()
+        orm.commit()
         return new_author.id
 
 @orm.db_session
